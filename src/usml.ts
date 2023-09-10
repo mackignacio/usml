2e58a0961f2118f34e0dfc320eb010a0719a16b1
Mack Ignacio <mack_ignacio@yahoo.com>
2023-09-10 16:43:41 +0800

    feat(core): :sparkles: add basic implementation of usml

--- src/usml.ts
+++ src/usml.ts
@@ -1,1 +1,25 @@
-console.log("Welcome to USML");
+import parser from "./lib/parser";
+import constants from "./utils/constants";
+
+// https://stackoverflow.com/questions/52150531/is-it-possible-to-read-the-html-body-before-the-dom-ready-event
+document.addEventListener(constants.DOM.EVENT.READY_STATE, function (): void {
+  // document is loading, does not fire here since no change from "loading" to "loading"
+  if (document.readyState === constants.DOM.LOADING) {
+    // TODO: usml.init()
+  }
+
+  // document fully read. Fires before DOMContentLoaded
+  if (document.readyState === constants.DOM.READY) {
+    return parser(document.body);
+  }
+
+  if (document.readyState == constants.DOM.LOADED) {
+    //document fully read and all resources (like images) loaded. fires after DOMContentLoaded
+    // TODO: usml.afterLoad();
+  }
+});
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+document.addEventListener(constants.DOM.EVENT.CONTENT_LOADED, () => {
+  // TODO: usml.contentLoad()
+});
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,9 +3,21 @@ import {
 } from "./types";
 
 const USML_LOAD: USMLLoad = "load";
+
+const DOM = {
+  LOADING: "loading",
+  READY: "interactive",
+  LOADED: "complete",
+  EVENT: {
+    READY_STATE: "readystatechange",
+    CONTENT_LOADED: "DOMContentLoaded",
+  },
+};
+
 const USML_ACTION: USMLAction[] = [USML_LOAD];
 
 export default {
   USML_ACTION,
   USML_LOAD,
+  DOM,
 };
